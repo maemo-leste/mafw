af39b3a7d8fd20d61512311aca84112a74772626
mtapoja <mtapoja@ee4951f8-51a9-4163-9227-c8c91700b67f>
2010-01-31 18:58:59 +0000

    MAFW PR 1.2 pre-release 2010.05-1.

--- mafw-gst-renderer/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
-
-export AUTOMAKE="automake-1.9"
-export ACLOCAL=`echo $AUTOMAKE | sed s/automake/aclocal/`
-
-autoreconf -v -f -i || exit 1
-test -n "$NOCONFIGURE" || ./configure \
-	--enable-debug --enable-maintainer-mode "$@"
